1314d28d034c5253909446d7f77baba48f2bf298
RamithaHeshan33 <st20286184@outlook.cardiffmet.ac.uk>
2024-04-02 20:40:21 +0530

    Second day

--- HospitalManagementSystem/src/hospitalmanagementsystem/FXMLDocumentController.java
+++ HospitalManagementSystem/src/hospitalmanagementsystem/FXMLDocumentController.java
@@ -9,7 +9,14 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -17,6 +24,53 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    //LETS NAME ALL COMPONENTS WE HAVE ON ADMIN PAGE
+    
+     @FXML
+    private CheckBox login_checkBox;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private Button login_loginBtn;
+
+    @FXML
+    private PasswordField login_password;
+
+    @FXML
+    private Hyperlink login_registerHere;
+
+    @FXML
+    private ComboBox<?> login_user;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private CheckBox register_checkBox;
+
+    @FXML
+    private TextField register_email;
+
+    @FXML
+    private AnchorPane register_form;
+
+    @FXML
+    private Hyperlink register_loginHere;
+
+    @FXML
+    private PasswordField register_password;
+
+    @FXML
+    private Button register_signupBtn;
+
+    @FXML
+    private TextField register_username;
+
     
     
     @Override
