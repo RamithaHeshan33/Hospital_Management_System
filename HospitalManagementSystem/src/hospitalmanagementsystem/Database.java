f7e8d6c964009bc863555dc15cfcda6a79896117
RamithaHeshan33 <st20286184@outlook.cardiffmet.ac.uk>
2024-04-04 18:32:44 +0530

    Day 03

--- HospitalManagementSystem/src/hospitalmanagementsystem/Database.java
+++ HospitalManagementSystem/src/hospitalmanagementsystem/Database.java
@@ -15,9 +15,9 @@ public class Database {
 
     public static Connection connectDB() {
     try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/hospital", "root", "");
+        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3307/hospital", "root", "root");
         return connect;
     } catch (Exception e) {
         System.out.println("Error connecting to the database: " + e);
--- HospitalManagementSystem/src/hospitalmanagementsystem/FXMLDocumentController.java
+++ HospitalManagementSystem/src/hospitalmanagementsystem/FXMLDocumentController.java
@@ -75,21 +75,105 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextField register_username;
     
+    @FXML
+    private TextField register_showPassword;
+    
+    @FXML
+    private TextField login_showPassword;
+    
     private Connection connect;
     private PreparedStatement prepare;
     private ResultSet result;
     
     private AlertMessage alert = new AlertMessage();
     
+    
+    public void loginAccount() {
+        if(login_username.getText().isEmpty() || login_password.getText().isEmpty()) {
+            alert.errorMessage("Fields cannot be blank");
+        }
+        
+        else {
+            String sql = "SELECT * FROM admin WHERE username = ? AND password = ?";
+            
+            connect = Database.connectDB();
+            
+            try {
+                
+                if(!login_showPassword.isVisible()) {
+                    if(!login_showPassword.getText().equals(login_password.getText())) {
+                        login_showPassword.setText(login_password.getText());
+                    }
+                }
+                
+                else {
+                    if(!login_showPassword.getText().equals(login_password.getText())) {
+                        login_password.setText(login_showPassword.getText());
+                    }
+                }
+                
+                prepare = connect.prepareStatement(sql);
+                prepare.setString(1, login_username.getText());
+                prepare.setString(2, login_password.getText());
+                result = prepare.executeQuery();
+                
+                if(result.next()) {
+                    alert.successMessage("Login Successfully!");
+                }
+                
+                else {
+                    alert.errorMessage("Incorrect Username or Password");
+                }
+                
+                
+            }
+            
+            catch(Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+    
+    public void loginShowPassword() {
+        if(login_checkBox.isSelected()) {
+            login_showPassword.setText(login_password.getText());
+            login_showPassword.setVisible(true);
+            login_password.setVisible(false);
+        }
+        
+        else {
+            login_password.setText(login_showPassword.getText());
+            login_showPassword.setVisible(false);
+            login_password.setVisible(true);
+        }
+    }
+    
+    
     public void registerAccount() {
         if (register_email.getText().isEmpty() || register_username.getText().isEmpty() || register_password.getText().isEmpty()) {
             alert.errorMessage("Fileds cannot be blank");
-        } else {
+        } 
+        
+        else {
             String checkUsername = "SELECT * FROM admin WHERE username = '" + register_username.getText() + "'";
             
             connect = Database.connectDB();
             
             try {
+                
+                if(!register_showPassword.isVisible()) {
+                    if(!register_showPassword.getText().equals(register_password.getText())) {
+                        register_showPassword.setText(register_password.getText());
+                    }
+                }
+                
+                else {
+                    if(!register_showPassword.getText().equals(register_password.getText())) {
+                        register_password.setText(register_password.getText());
+                    }
+                }
+                
+                
                 prepare = connect.prepareStatement(checkUsername);
                 result = prepare.executeQuery();
                 
@@ -97,8 +181,12 @@ public class FXMLDocumentController implements Initializable {
                     alert.errorMessage(register_username.getText() + "is already exist!!");
                 }
                 
+                else if (register_password.getText().length() < 8) {
+                    alert.errorMessage("Invalid Message!! At least 8 characters required!!");
+                }
+                
                 else {
-                    String insertDataa = "INSERT INTO admin (email, username, password, date) VALUES (?,?,?,?)";
+                    String insertDataa = "INSERT INTO admin (email, username, password, data) VALUES (?,?,?,?)";
 
                     Date date = new Date();
                     java.sql.Date sqlDate = new java.sql.Date(date.getTime());
@@ -112,12 +200,37 @@ public class FXMLDocumentController implements Initializable {
                     prepare.executeUpdate();
                     
                     alert.successMessage("Registration Successful");
+                    registerClear();
+                    
+                    login_form.setVisible(true);
+                    register_form.setVisible(false);
 
                 }
                 
             } catch (Exception e) {
                 System.out.println("ssssssssssss" +e);
             }
+        }
+    }
+    
+    public void registerClear() {
+        register_email.clear();
+        register_username.clear();
+        register_password.clear();
+        register_showPassword.clear();
+    }
+    
+    public void registerShowPassword() {
+        if(register_checkBox.isSelected()){
+            register_showPassword.setText(register_password.getText());
+            register_showPassword.setVisible(true);
+            register_password.setVisible(false);
+        }
+        
+        else {
+            register_password.setText(register_showPassword.getText());
+            register_showPassword.setVisible(false);
+            register_password.setVisible(true);
         }
     }
     
